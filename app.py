1746f63f974c333dc758166c75f647be3a01fb10
pasi751 <pasindumadusara751@gmail.com>
2024-03-19 22:34:59 +0530

    app updated

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask
 from flask_cors import CORS
 from models import db
@@ -7,29 +6,23 @@ from routes.documentation_routes import documentation_bp
 from routes.prompt_generator_routes import prompt_generator_bp
 from flask_jwt_extended import JWTManager
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://genify:g3nify123$@genifydatabase.mysql.database.azure.com/genify'
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://genify:g3nify123$@genifydatabase.mysql.database.azure.com/genify'
 
-    # Configuration for JWT
-    app.config['JWT_SECRET_KEY'] = 'yX*F2wRpE78Ct!LkG$mjQvZpF9p#bS%z'  # Change this to a secret key
-    jwt = JWTManager(app)
+# Configuration for JWT
+app.config['JWT_SECRET_KEY'] = 'yX*F2wRpE78Ct!LkG$mjQvZpF9p#bS%z'  # Change this to a secret key
+jwt = JWTManager(app)
 
-    
-    db.init_app(app)  # Register SQLAlchemy extension with the Flask app
+db.init_app(app)  # Register SQLAlchemy extension with the Flask app
 
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
-    app.register_blueprint(users_bp)
-    app.register_blueprint(documentation_bp)
-    app.register_blueprint(prompt_generator_bp)
-
-
-    return app
+app.register_blueprint(users_bp)
+app.register_blueprint(documentation_bp)
+app.register_blueprint(prompt_generator_bp)
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
